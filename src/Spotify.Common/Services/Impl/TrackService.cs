1ccce8d10227af8b739b0daded6090f0fa19836c
Jason Farrell <jfarrell@westmonroepartners.com>
2016-10-08 18:32:30 -0500

    Added Tracks Part 2

--- src/Spotify.Common/Services/Impl/TrackService.cs
+++ src/Spotify.Common/Services/Impl/TrackService.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Spotify.Common.Models;
+
 namespace Spotify.Common.Services.Impl
 {
-    public class TrackService
+    public class TrackService : ITrackService
     {
-        public TrackService()
+        private List<Track> _cache;
+
+        public List<Track> AvailableTracks
         {
+            get
+            {
+                if (_cache == null)
+                    return new List<Track>();
+
+                return _cache;
+            }
+        }
+
+        public async Task<bool> LoadTracksForAlbum(string albumId)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var urlString = $"https://api.spotify.com/v1/albums/{albumId}/tracks";
+
+                    var response = await client.GetAsync(urlString);
+                    var content = await response.Content.ReadAsStringAsync();
+                    var root = JObject.Parse(content);
+
+
+                    _cache = JsonConvert.DeserializeObject<List<Track>>(root["items"].ToString());
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
